13ab611b4575cc52307e6bd7be8921ee2516f85f
Marco H <65416881+MarcoHam@users.noreply.github.com>
2020-06-10 18:05:27 +0200

    Add files via upload

---
+++ 10_javascript_introduction/00_script.js
@@ -0,0 +1,4 @@
+console.log("Hello, SmartNinja2!");
+console.log("Hello World!");
+console.log("Test1");
+console.log("Test2");
---
+++ 10_javascript_introduction/03_ifelse.js
@@ -0,0 +1,14 @@
+let mood = "sad";
+
+if (mood === "happy") {
+    console.log("HAPPY");
+} else {
+    console.log("Oh nooooooo");
+}
+
+
+/* 
+AUFGABE
+1) Defintion einer Variable als Bedingung
+2) Programmiere eine If-Else-Struktur für diese Bedingung
+*/
